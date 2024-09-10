0b8d174c6d714356443eb2cc6ce7b15985093b6f
SinnsykFG <143673271+SinnsykFG@users.noreply.github.com>
2024-09-10 08:11:31 -0300

    semana-4-JS

---
+++ JS/script.js
@@ -0,0 +1,36 @@
+function actReloj(){
+   let hhmmss=new Date();
+   let horas=hhmmss.getHours();
+   let minutos=hhmmss.getMinutes();
+   let segundos=hhmmss.getSeconds();
+   let lahora="";
+   let dia=hhmmss.getDate();
+   let mes=hhmmss.getMonth()+1;
+   let año=hhmmss.getFullYear();
+
+   horas = (horas<=9)?("0"+horas):horas;
+   minutos = (minutos<=9)?("0"+minutos):minutos;
+   segundos=(segundos<=9)? ("0"+segundos):segundos;
+   dia=(dia<=9)?("0"+dia):dia;
+   mes=(mes<=9)?("0"+mes):mes;        
+
+    lahora=horas+":"+minutos+":"+segundos;
+    lafecha=dia+"/"+mes+"/"+año;
+    
+
+   reloj = document.getElementById('reloj')
+   reloj.innerHTML=lahora+" - "+lafecha;
+
+
+
+   
+   // Formato deseado (por ejemplo, DD/MM/YYYY HH:MM:SS)
+   let formattedDate = `${day}/${month}/${year}`;
+   const formattedTime = `${lahora}`;
+
+    // Mostrar fecha y hora
+    document.getElementById('fecha').innerHTML = formattedDate;
+
+   setTimeout("actReloj()",1000);
+}
+
